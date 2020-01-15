de1899a327400342ccb9172c46d95fdba7a9e746
EMartinTaylor <42308492+emartintaylor@users.noreply.github.com>
2020-01-14 23:31:06 -0500

    Initial chat

---
+++ app/src/main/java/com/nagesagi/groupchat/Message/MessageViewHolder.java
@@ -0,0 +1,10 @@
+package com.nagesagi.groupchat.Message;
+
+import android.view.View;
+import android.widget.TextView;
+
+public class MessageViewHolder{
+    public View avatar;
+    public TextView name;
+    public TextView messageBody;
+}
